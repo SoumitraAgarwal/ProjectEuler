74846ebdf6f8ba5c05f46f0887eccd6ea7247aad
SoumitraAgarwal <agarwalsoumitra1504@gmail.com>
2017-12-12 23:42:33 +0530

    Solution 7

---
+++ 7.cpp
@@ -0,0 +1,51 @@
+# include <bits/stdc++.h>
+long long check = 600851475143;
+
+int primes[1000000] = {0}, arra[1000000] = {0};
+
+int seive()
+{
+	int k = 1, i, j;
+	primes[0] = 2;
+	for(i = 3; i<=1000; i+= 2)
+	{
+		if(arra[i] == 0)
+		{
+			for(j = 2*i; j<1000000; j+=i)
+			{
+				arra[j] = 1;
+			}
+			primes[k] = i;
+			k ++;
+		}
+	}
+	for(; i<1000000; i+= 2)
+	{
+		if(arra[i] == 0)
+		{
+			primes[k] = i;
+			k++;
+		}
+	}
+	return k;
+}
+
+int main()
+{
+	int ans = 0, i;
+	int k = seive();
+	// printf("%d\n", k);
+	for(i = 0; i<k; i++)
+	{
+		if(primes[i]>sqrt(check))
+		{
+			break;
+		}
+		if(check%primes[i] == 0)
+		{
+			ans = primes[i];
+		}
+	}
+	printf("%d\n", primes[10000]);
+	return 0;
+}
